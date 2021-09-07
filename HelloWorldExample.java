41f6dc7ac47479d9e0284f2f3f4a11b8b738651d
L00162879 <77925018+L00162879@users.noreply.github.com>
2021-09-07 12:17:16 +0100

    Create HelloWorldExample.java

---
+++ HelloWorldExample.java
@@ -0,0 +1,8 @@
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		System.out.println("2021 LYIT MSc in DevOps - Jenkins Scripting by Juarez Alvares Barbosa Junior");
+
+	}
+
+}
