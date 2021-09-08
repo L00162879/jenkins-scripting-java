d28c4908b84df6034dc0988d37bb9ad0da6cc8fb
Juarez A. Barbosa Jr <77925018+L00162879@users.noreply.github.com>
2021-09-08 09:36:49 +0100

    Update HelloWorldExample.java

--- HelloWorldExample.java
+++ HelloWorldExample.java
@@ -1,5 +1,15 @@
+/**
+ * 
+ * @author Juarez Alvares Barbosa Junior - MSc Student @ LYIT
+ * @version 1.0
+ *
+ */
 public class HelloWorldExample {
 
+	/**
+	 * Main entry point for our Hello World Example testing class
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("2021 LYIT MSc in DevOps - Jenkins Scripting by Juarez Alvares Barbosa Junior");
 
